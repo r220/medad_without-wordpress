1b705f7bc3a1c507064e204d11cccabb0b191541
reem <upontime000@gmail.com>
2024-09-21 16:26:32 +0300

    j

--- home.js
+++ home.js
@@ -1,43 +1,62 @@
-var splide = new Splide('.splide', {
+// RIVIEWS
+var splide_reviews = new Splide('#splide_reviews', {
   type: 'loop',
   direction: 'rtl',
-  height: '22rem',
   autoWidth: true,
-  speed: 600,
-  padding: '1rem',
-  pagination: !1,
-  // focus: 'center',
-
+  pagination: false,
   drag: 'free',
-  autoScroll: {
-    speed: 1,
+  autoplay: true,
+  interval: 4000,
+  speed: 600,
+  gap: 20,
+  perPage: 4,
+  height: "21rem",
+  focus: 4,
+  padding: {
+    left: 20,
+    right: 40,
   },
+
   breakpoints: {
     1280: {
       gap: 20,
       perPage: 4,
-      autoHeight: true,
-      padding: {
-        left: 15,
-        right: 15,
-      }
-    },
-    1000: {
-      gap: 40,
-      perPage: 4,
+      height: "21rem",
+      focus: 4,
       padding: {
         left: 20,
         right: 20,
-      }
+      },
+    },
+    1000: {
+      gap: 20,
+      perPage: 4,
+      height: "21rem",
+      focus: 4,
+      padding: {
+        left: 20,
+        right: 20,
+      },
     },
     992: {
-      gap: 40,
-      perPage: 4,
+      gap: 20,
+      perPage: 3,
+      height: "22rem",
+      focus: 'center',
+      padding: {
+        left: 13,
+        right: 13,
+      }
     },
-    600: {
-      gap: 40,
+    768: {
+      gap: 25,
       perPage: 2,
-      height: "23rem",
+      height: "24rem",
+      focus: 2,
+      padding: {
+        left: 30,
+        right: 24,
+      }
     },
     557: {
       gap: 20,
@@ -46,63 +65,83 @@ var splide = new Splide('.splide', {
       focus: 'center'
     }
   }
-
-
-
-
-  // type: "fade",
-  // direction: 'rtl',
-  // perPage: 4,
-  // // focus: !1,
-  // perMove: 2,
-  // gap: 20,
-  // pagination: !1,
-  // autoplay: !(0,
-  // i.s2)(),
-  // breakpoints: {
-  //     1280: {
-  //         gap: 15,
-  //         perPage: 4,
-  //         padding: {
-  //             left: 50,
-  //             right: 15
-  //         }
-  //     },
-  //     1200: {
-  //         gap: 15,
-  //         perPage: 4,
-  //         padding: {
-  //             left: 50,
-  //             right: 15
-  //         }
-  //     },
-  //     992: {
-  //         gap: 15,
-  //         perPage: 3,
-  //         padding: {
-  //             left: 50,
-  //             right: 15
-  //         }
-  //     },
-  //     768: {
-  //         gap: 15,
-  //         perPage: 2,
-  //         padding: {
-  //             left: 50,
-  //             right: 15
-  //         }
-  //     },
-  //     557: {
-  //         gap: 10,
-  //         perPage: 1,
-  //         padding: {
-  //             left: "4rem",
-  //             right: 15
-  //         }
-  //     }
-  // }
 });
 
 
 
-splide.mount();
+splide_reviews.mount();
+
+// PRODUCTS
+var splide_popular_products = new Splide('#splide_popular_products', {
+  type: 'loop',
+  direction: 'rtl',
+  autoWidth: true,
+  pagination: false,
+  drag: 'free',
+  autoplay: true,
+  interval: 4000,
+  speed: 600,
+  gap: 30,
+  perPage: 3,
+  height: "30rem",
+  focus: 3,
+  padding: {
+    left: 30,
+    right: 60,
+  },
+
+  breakpoints: {
+    1280: {
+      gap: 40,
+      perPage: 3,
+      height: "30rem",
+      focus: 3,
+      // padding: {
+      //   left: 20,
+      //   right: 20,
+      // },
+    },
+    1000: {
+      gap: 40,
+      perPage: 4,
+      height: "28rem",
+      focus: 4,
+      padding: {
+        left: 20,
+        right: 20,
+      },
+    },
+    992: {
+      gap: 30,
+      perPage: 2,
+      height: "28rem",
+      focus: 2,
+      padding: {
+        left: 70,
+        right: 70,
+      }
+    },
+    768: {
+      gap: 25,
+      perPage: 2,
+      height: "28rem",
+      focus: 2,
+      padding: {
+        left: 30,
+        right: 24,
+      }
+    },
+    600: {
+      gap: 20,
+      perPage: 1,
+      height: "40rem",
+      focus: 'center'
+    }
+  }
+});
+
+
+
+splide_popular_products.mount();
+
+
